ff43b77ad1f187d228d57556a25b33772b8402e8
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-20 09:06:26 +0530

    Add: Java Program to Check Whether a Number is Even or Odd

---
+++ src/com/bridgelabz/basiccoreprogram/EvenOrOdd.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.basiccoreprogram;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		int a = 4;
+		if (a % 2 == 0) {
+			System.out.println(a + " is a Even Number");
+		} else {
+			System.out.println(a +" is a Odd Number");
+		}
+	}
+}
