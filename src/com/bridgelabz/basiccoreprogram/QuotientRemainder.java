32f8e1355fa24f6fae065e088e5b7578ff4bb5aa
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-20 09:15:56 +0530

    Add:Java Program to Compute Quotient and Remainder

---
+++ src/com/bridgelabz/basiccoreprogram/QuotientRemainder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.basiccoreprogram;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		
+		int dividend=25;
+		int divisor=4;
+		int quotient=dividend/divisor;
+		int remainder=dividend%divisor;
+		System.out.println("Quotient is: "+quotient);
+		System.out.println("Remainder is: "+remainder);
+	}
+}
