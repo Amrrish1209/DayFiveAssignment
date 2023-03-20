1391f4cc61df2733280afabdea733471ca061bde
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-20 09:16:39 +0530

    Add:Java Program to Swap Two Numbers

---
+++ src/com/bridgelabz/basiccoreprogram/SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.basiccoreprogram;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a=100;
+		int b=200;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("the a value is: "+a+" The b value is: "+b);
+	}
+}
